0376342b41f9e3e4d2fa55afe631980395a35c6e
Martin McGreal <mmmcgrea@amazon.com>
2021-03-08 13:02:01 -0600

    Add defaults and env vars for deployment env

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python3
 
+import os
 from aws_cdk import core
 
 from cdk_vpc_ec2.cdk_vpc_stack import CdkVpcStack
 from cdk_vpc_ec2.cdk_ec2_stack import CdkEc2Stack
 from cdk_vpc_ec2.cdk_rds_stack import CdkRdsStack
 
+# Defaults
+APPENV_DEFAULT_ACCOUNT = "964152837058"
+APPENV_DEFAULT_REGION = "us-east-2"
+
+# Override defaults
+APPENV_ACCOUNT = os.environ.get("APPENV_ACCOUNT") or APPENV_DEFAULT_ACCOUNT
+APPENV_REGION = os.environ.get("APPENV_REGION") or APPENV_DEFAULT_REGION
+
 app = core.App()
 
-env_USA = core.Environment(account="887350810418", region="us-east-2")
+env_USA = core.Environment(account=APPENV_ACCOUNT, region=APPENV_REGION)
 
 vpc_stack = CdkVpcStack(app, "cdk-vpc", env=env_USA)
 ec2_stack = CdkEc2Stack(app, "cdk-ec2",
